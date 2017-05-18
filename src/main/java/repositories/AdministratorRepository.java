4949cebb493316baa356a9d368e2dd52f157b9f2
David De la torre <daviddelatorre@Air-de-David.local>
2017-05-18 19:51:38 +0200

    queries a saco

--- src/main/java/repositories/AdministratorRepository.java
+++ src/main/java/repositories/AdministratorRepository.java
@@ -1,9 +1,11 @@
 package repositories;
 
-import domain.Administrator;
+import domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 /**
  * Created by daviddelatorre on 28/3/17.
@@ -13,6 +15,117 @@ public interface AdministratorRepository extends JpaRepository<Administrator, In
 
     @Query("select c from Actor c where c.userAccount.id = ?1")
     Administrator findByUserAccountId(int userAccountId);
+
+
+    //*************************** LEVEL C ***************************
+
+
+    @Query("select avg(questions.size) from User")
+    Double averageNumberOfQuestionPerUser();
+
+    @Query("select max(questions.size) from User")
+    int maxNumberOfQuestionPerUser();
+
+    @Query("select min(questions.size) from User")
+    int minNumberOfQuestionPerUser();
+
+    @Query("select avg(answers.size) from User")
+    Double averageNumberOfAnswerPerUser();
+
+    @Query("select max(answers.size) from User")
+    int maxNumberOfAnswerPerUser();
+
+    @Query("select min(answers.size) from User")
+    int minNumberOfAnswerPerUser();
+
+    @Query("select u from User u order by u.answers.size asc")
+    User userWhoHaveAutoredMoreAnswer();
+
+    @Query("select u from User u order by u.questions.size asc  ")
+    Collection<User> userSortedByQuestionNumber();
+
+    @Query("select u from User u join u.answers a order by a.likes")
+    Collection<User> userSortedByNumberOfLikes();
+
+    @Query("select u from User u join u.answers a order by a.dislikes")
+    Collection<User> userSortedByNumberOfDislikes();
+
+
+
+    //*************************** LEVEL B ***************************
+
+    @Query("select u from User u where u.creditCard is not null")
+    Collection<User> usersWithCreditCards();
+
+    Teacher teacherWithMoreWebinars();
+    Teacher teacherWithLessWebinars();
+
+
+    @Query("select avg(webinars.size) from Teacher ")
+    Double averageNumberOfWebinarsPerTeacher();
+
+    @Query("select t from Teacher t order by t.webinars.size")
+    Collection<Teacher> teachersSortedByNumberOfWebinars();
+
+    Webinar webinarWithMoreRegisteredUsers();
+    Webinar webinarWithLessRegisteredUsers();
+
+    @Query("select w from Webinar w order by w.partakers.size")
+    Collection<Webinar> webinarSortedByNumberOfAssistance();
+
+    @Query("select avg(comments.size) from Webinar ")
+    Double averageNumberOfCommentsPerWebinar();
+
+    @Query("select max(comments.size) from Webinar ")
+    int maxNumberOfCommentPerWebinar();
+
+    @Query("select min(comments.size) from Webinar ")
+    int minNumberOfCommentPerWebinar();
+
+    @Query("select u from User u join u.questions q where q.banned = true order by u.questions.size")
+    Collection<User> userSortedByNumberOfQuestionBanned();
+
+    @Query("select u from User u join u.answers q where q.banned = true order by u.answers.size")
+    Collection<User> userSortedByNumberOfAnswerBanned();
+
+    User userwithMoreQuestionBanned();
+    User userWithLessQuestionBanned();
+    @Query("select avg(u.questions.size) from User  u join u.questions q where q.banned = true")
+    Double averageNumberOfBannedQuestionPerUser();
+
+
+    //*************************** LEVEL A ***************************
+
+
+    User userwithMoreAnswerBanned();
+    User userWithLessAnswerBanned();
+    Double averageNumberOfBannedAnswerPerUser();
+
+
+    Double percetageOfBannedQuestions();
+    Double percentageOfBannedAnswers();
+    Double percentageOfBannedUser();
+
+    Collection<Actor> actorSortedByNumberOfSendMessage();
+    Collection<Actor> actorSortedByNumberOfReceivedMessages();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     //A listing with the number of chorbies per country and city.
